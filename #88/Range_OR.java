f97b54bb7cf8287f6d9def7dbdfd83dd017bfaff
Ashish <ashishmaurya9193@gmail.com>
2023-02-06 01:40:36 +0530

    tet

---
+++ #88/Range_OR.java
@@ -0,0 +1,13 @@
+package #88;
+
+class Solution{
+    public int rangeOR(int n){
+        int ans = 1;
+        while(ans<=n){
+            ans*=2;
+            
+        }
+        ans--;
+        return ans;
+    }
+}
